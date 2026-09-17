24baadbdec21440db78c29f53103c5910c49f0d4
Darshan BG <darshan.bg.23@gmail.com>
2026-09-17 22:10:25 +0530

    Max Con 1s Solutions

--- Arrays/Sliding Window/4 - Maximum Number of Consecutive 1s/brute-force-solution.py
+++ Arrays/Sliding Window/4 - Maximum Number of Consecutive 1s/brute-force-solution.py
@@ -1,1 +1,20 @@
-﻿
+﻿class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        
+        max_count = 0
+        
+        for i in range(n):
+            
+            if nums[i] == 0:
+                continue
+            
+            for j in range(i,n):
+                
+                if nums[j] == 0:
+                    break
+                
+                max_count = max(max_count, j-i+1)
+                
+        return max_count
